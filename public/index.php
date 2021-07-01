37c8f37717a40228e273cedd330069f789b71dea
VictorDesiderio <75548165+VictorDesiderio@users.noreply.github.com>
2021-07-01 15:44:57 -0500

    lll
    lll

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ define('LARAVEL_START', microtime(true));
 | instead of starting the framework, which could cause an exception.
 |
 */
-
+llllllllllllllllllllll
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
 }
